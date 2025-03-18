1b4eb9a5e1e9fa04302096e41dc225f51edf85ad
Thiago Pontes Soares <thiagodevps@gmail.com>
2025-03-18 15:12:52 -0300

    fix - remove dots

--- src/components/AboutMe.tsx
+++ src/components/AboutMe.tsx
@@ -14,7 +14,7 @@ export function AboutMe(){
     <div className={styles.aboutMeContainer}>
       <p>Olá, sou Thiago Pontes Soares, sou um desenvolvedor Back-end.</p>
       <p>Possuo experiência com desenvolvimento em geral (front-end & back-end), trabalhando com tecnologias como PHP, HTML, CSS, JavaScript (NodeJS, ReactJS).</p>
-      <p>“Sou um profissional muito dedicado e disposto a aprender novas tecnologias a fim de aprimorar minhas habilidade, buscando sempre dar o meu melhor no projeto ou organização na qual estou inserido.”</p>
+      <p>Sou um profissional muito dedicado e disposto a aprender novas tecnologias a fim de aprimorar minhas habilidade, buscando sempre dar o meu melhor no projeto ou organização na qual estou inserido.</p>
     </div> 
     <div className={styles.aboutMeSchoolContainer}>
       <p><img src='/fatec.png' className={styles.fatec}/>Análise e Desenvolvimento de Sistemas, FATEC Ourinhos (2018-2020).</p>
